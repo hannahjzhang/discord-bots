a5665873b61d3544eb76fa83106739fad5f6c7a5
Hannah Zhang <hannahzhang@Hannahs-Air.hsd1.ca.comcast.net>
2020-11-13 20:47:12 -0800

    keyword & capability changes

--- hello_world_bot.py
+++ hello_world_bot.py
@@ -1,22 +1,35 @@
 import discord
 import hello_world_key
 
-#create discord client
+TOKEN = hello_world_key.KEY
+# create discord client
 client = discord.Client()
 
 @client.event
+async def on_ready():
+  print(f'{client.user.name} has connected to Discord')
+
+# respond to certain messages
+@client.event
 async def on_message(message):
-  #check if message is from ourselves to avoid infinite loop
+  # check if message is from ourselves to avoid infinite loop
   if message.author == client.user:
     return
-
-  if message.content == ('!hello'):
+  if message.content == ('hello'):
     await message.channel.send('world')
-    await message.add_reaction('🌎')
-
-  if message.content == ('!world'):
+    await message.add_reaction('🌝')
+  if message.content == ('world'):
     await message.channel.send('oh hello')
-    await message.add_reaction('👋')
+    await message.add_reaction('🌚')
+
+# respond to certain reactions
+@client.event
+async def on_reaction_add(reaction, user):
+  react = str(reaction)
+  if react == "👋":
+    await channel.send('hello to you too')
+  if react == '🌎':
+    await channel.send('world hello world')
 
 # to run the bot
-client.run(hello_world_key.KEY)
+client.run(TOKEN)
